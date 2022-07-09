f805faa02eacd769d0d8dcce4afb13367c53d537
hjl <2764881569@qq.com>
2022-07-09 15:30:18 +0800

    Rank

--- frontend/src/pages/Rank/index.jsx
+++ frontend/src/pages/Rank/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import { Container } from './style'
+import { Container, List, ListItem, SongList } from './style'
 import Scroll from '@/components/common/Scroll'
 import { EnterLoading } from '@/pages/Singers/style'
 import Loading from '@/components/common/loading'
@@ -25,11 +25,40 @@ function Rank(props) {
   }, [])
   let globalStartIndex = filterIndex(rankList)
   let officalList = rankList.slice(0, globalStartIndex);
-  console.log(globalStartIndex, officalList, rankList)
-  const renderRankList = () => {
+  let globalList = rankList.slice(globalStartIndex);
+  // console.log(globalStartIndex, officalList, rankList)
+  const enterDetail = (detail) => {
+    props.history.push(`/rank/${detail.id}`)
+  }
+  const renderSongList = (list) => {
+    return list.length ? (
+      <SongList>
+        {
+          list.map((item, index) => {
+            return <li key={index}>{index+1}. {item.first} - {item.second}</li>
+          })
+        }
+      </SongList>
+    ) : null;
+  }
+  const renderRankList = (list, global) => {
     return (
-      <div>
-      </div>
+      <List globalRank={global}>
+       {
+        list.map((item, index) => {
+          return (
+            <ListItem key={`${item.coverImgId}${index}`} tracks={item.tracks} onClick={() => enterDetail(item)}>
+              <div className="img_wrapper">
+                <img src={item.coverImgUrl} alt=""/>
+                <div className="decorate"></div>
+                <span className="update_frequecy">{item.updateFrequency}</span>
+              </div>
+              { renderSongList(item.tracks)  }
+            </ListItem>
+          )
+       })
+      } 
+      </List>
     )
   }
   let displayStyle = enterLoading?{"display": "none"}: {"display": ""};
@@ -38,7 +67,9 @@ function Rank(props) {
       <Scroll>
         <div>
           <h1 className="offical" style={displayStyle}>官方榜</h1>
-          { renderRankList(officalList) }
+            { renderRankList(officalList) }
+          <h1 className="global" style={displayStyle}>全球榜</h1>
+            { renderRankList(globalList, true) }
         {enterLoading && <EnterLoading><Loading></Loading></EnterLoading>}
         </div>
       </Scroll>
